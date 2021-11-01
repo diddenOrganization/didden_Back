99f3377a88ae38b7bccfe1c30076bac632fdd213
xxxx-jeung <bioman3238@gmail.com>
2021-11-01 13:38:57 +0900

    CrossOrigin

--- src/main/java/com/diden/demo/DemoApplication.java
+++ src/main/java/com/diden/demo/DemoApplication.java
@@ -4,10 +4,12 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
 @ComponentScan("com.diden")
 @MapperScan("com.diden.*.mapper*")
+@CrossOrigin("*")
 public class DemoApplication {
 
 	public static void main(String[] args) {
