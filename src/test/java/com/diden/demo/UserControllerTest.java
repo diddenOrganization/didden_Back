a5e2b2a1785fb280f78bacc71ef2e6f8764706c9
xxxx-jeung <bioman3238@gmail.com>
2022-03-07 17:47:37 +0900

    테스트코드 정상작동 확인

--- src/test/java/com/diden/demo/UserControllerTest.java
+++ src/test/java/com/diden/demo/UserControllerTest.java
@@ -6,32 +6,51 @@ import java.util.List;
 import com.diden.config.JwtTokenProvider;
 import com.diden.config.vo.TokenVo;
 import com.diden.user.service.UserService;
+import com.diden.user.service.impl.UserServiceImpl;
 import com.diden.user.vo.UserVo;
 import com.diden.utils.JwtTokenUtil;
 import com.diden.utils.ParsingJson;
 
+import org.apache.catalina.User;
 import org.assertj.core.util.Arrays;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jws;
 import io.jsonwebtoken.Jwts;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
-@ExtendWith(MockitoExtension.class)
+@SpringBootTest
 public class UserControllerTest {
-    @Spy
-    JwtTokenUtil jwtTokenUtil;
-
-    @Spy
+    @Autowired
     UserService userService;
 
-    @Spy
-    JwtTokenProvider jwtTokenProvider;
-
     @Test
+    // @Transactional
+    public void 토큰생성() {
+        UserVo userVo = new UserVo();
+        userVo.setUserId("test");
+        userVo.setUserPassword("test");
+
+        UserVo resultUserVo = userService.userInfo(userVo);
+        System.out.println(resultUserVo);
+        /*TokenVo tokenVo = new TokenVo();
+        tokenVo.setAccessJwsToken(jwtTokenUtil.makeJwtAccToken(resultUserVo).getAccessJwsToken());
+        tokenVo.setRefreshJwsToken(jwtTokenUtil.makeJwtRefToken(resultUserVo).getRefreshJwsToken());
+
+        System.out.println(tokenVo);
+
+        토큰체크(tokenVo.getAccessJwsToken(), tokenVo.getRefreshJwsToken());*/
+    }
+
+    //@Test
     public void 토큰해제() {
         ParsingJson parsingJson = new ParsingJson();
         String accToken = "eeyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJpc3MiOiJhY2MiLCJzdWIiOiJURVNUIiwicmVzdWx0Ijp0cnVlLCJpYXQiOjE2Mzg1NDExOTksImV4cCI6MTYzODU0ODM5OX0.pkq_V2OQDIS5YUCN5EAjA7of4LMzUJcEdJsHCmJbL47vfE_A9um6H1ECQ-FGj8stszFoXW_0nQFmgJK7eIT6Dg";
@@ -49,22 +68,7 @@ public class UserControllerTest {
         System.out.println(parsingJson.stringToJsonObject(jsonPayload));
     }
 
-    @Test
-    // @Transactional
-    public void 토큰생성() {
-        UserVo userVo = new UserVo();
-        userVo.setUserId("test");
-        userVo.setUserPassword("test");
 
-        UserVo resultUserVo = userService.userInfo(userVo);
-        TokenVo tokenVo = new TokenVo();
-        tokenVo.setAccessJwsToken(jwtTokenUtil.makeJwtAccToken(resultUserVo).getAccessJwsToken());
-        tokenVo.setRefreshJwsToken(jwtTokenUtil.makeJwtRefToken(resultUserVo).getRefreshJwsToken());
-
-        System.out.println(tokenVo);
-
-        토큰체크(tokenVo.getAccessJwsToken(), tokenVo.getRefreshJwsToken());
-    }
 
     // @Test
     public void 토큰체크(String AccToken, String RefToken) {
