55ae193315f667f97ba47e1e43786e548894cad6
xxxx-jeung <bioman3238@gmail.com>
2022-03-23 05:52:54 +0900

    환경 구성중

--- src/main/java/com/diden/config/SpringConfig.java
+++ src/main/java/com/diden/config/SpringConfig.java
@@ -7,16 +7,16 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SpringConfig {
-    private final Announcement2Mapper announcementMapper;
-//    private final AnnouncementService announcementService;
-
-
-    public SpringConfig(Announcement2Mapper announcementMapper) {
-        this.announcementMapper = announcementMapper;
-    }
-
-    @Bean
-    public AnnouncementServiceImpl announcementService(){
-        return new AnnouncementServiceImpl(announcementMapper);
-    }
+//    private final Announcement2Mapper announcementMapper;
+////    private final AnnouncementService announcementService;
+//
+//
+//    public SpringConfig(Announcement2Mapper announcementMapper) {
+//        this.announcementMapper = announcementMapper;
+//    }
+//
+//    @Bean
+//    public AnnouncementServiceImpl announcementService(){
+//        return new AnnouncementServiceImpl(announcementMapper);
+//    }
 }
