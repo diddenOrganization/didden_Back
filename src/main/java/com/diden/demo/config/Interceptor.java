7030bbb8a92fb219a7c70c5ef65306421634859d
OhJeUng <bioman3238@gmail.com>
2022-06-12 14:40:52 +0900

    토큰 기능 수정
    토큰 기능 수정

--- src/main/java/com/diden/demo/config/Interceptor.java
+++ src/main/java/com/diden/demo/config/Interceptor.java
@@ -1,6 +1,9 @@
 package com.diden.demo.config;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 import javax.servlet.http.HttpServletRequest;
@@ -11,12 +14,21 @@ public class Interceptor implements HandlerInterceptor {
     final private TokenCheckAdepter tokenCheckAdepter = new TokenCheckAdepter();
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        final boolean result;
         log.info("Authorization : Interceptor");
         if (request.getServerName().startsWith("127") || request.getServerName().startsWith("local")) return true;
 
         final String loginType = request.getHeader("login_type");
         final String Authorization = request.getHeader("Authorization");
 
-        return this.tokenCheckAdepter.tokenCheckMethod(loginType, Authorization);
+        final JsonObject resultObject = this.tokenCheckAdepter.tokenCheckMethod(loginType, Authorization);
+        result = resultObject.get("result").getAsBoolean();
+
+        if(result) return true;
+        else {
+            response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+            response.getOutputStream().print(resultObject.toString());
+            return false;
+        }
     }
 }
