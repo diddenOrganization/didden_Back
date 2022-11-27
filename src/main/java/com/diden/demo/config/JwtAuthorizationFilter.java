a329072411f7f362e28c5464761c96829f030e39
OhJeUng <bioman3238@gmail.com>
2022-11-27 19:40:34 +0900

    로그 수정

--- src/main/java/com/diden/demo/config/JwtAuthorizationFilter.java
+++ src/main/java/com/diden/demo/config/JwtAuthorizationFilter.java
@@ -20,10 +20,19 @@ public class JwtAuthorizationFilter extends OncePerRequestFilter {
       final HttpServletRequest request, final HttpServletResponse response, final FilterChain chain)
       throws ServletException, IOException {
 
-    log.info(":: 요청 URI == {} ::", request.getRequestURI());
+    log.info(
+        ":::::::::::::::::::::::::::::::::::::: Request Start ::::::::::::::::::::::::::::::::::::::");
+    log.info(
+        "::::::::: request URI ==> [{}] {} :::::::::",
+        request.getMethod(),
+        request.getRequestURI());
+
     log.info(":: JwtAuthorizationFilter.doFilterInternal.loginTokenCheckMethod  ==  토큰 검증 시작 ::");
     if (this.tokenAdepterInterface.loginTokenCheckMethod(request)) { // 토큰 검증
       chain.doFilter(request, response);
     }
+
+    log.info(
+        ":::::::::::::::::::::::::::::::::::::: Request End ::::::::::::::::::::::::::::::::::::::");
   }
 }
